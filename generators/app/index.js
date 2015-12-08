eb41a2d2ae6a409c6a40f33e1984f21e9d152406
Frederik Hahne <frederik.hahne@gmail.com>
2015-12-08 21:17:58 +0100

    finish templating

--- generators/app/index.js
+++ generators/app/index.js
@@ -79,32 +79,18 @@ module.exports = yeoman.generators.Base.extend({
 
     this.apiDocResultType = this.props.apiDocResultType;
 
-    this.template('src/docs/asciidoc/_index.adoc', 'src/docs/asciidoc/index.adoc')
-    this.template('src/test/java/package/web/rest/_Swagger2MarkupTest.java', javaTestDir + 'web/rest/Swagger2MarkupTest.java')
-    this.template('src/docs/asciidoc/_index.adoc', 'src/docs/asciidoc/index.adoc')
+    this.template('src/docs/asciidoc/_index.adoc', 'src/docs/asciidoc/index.adoc');
+    this.template('src/test/java/package/web/rest/_Swagger2MarkupTest.java', javaTestDir + 'web/rest/Swagger2MarkupTest.java');
 
-    this.template('src/main/java/package/domain/_Fortune.java', javaDir + 'domain/Fortune.java');
-    this.template('src/main/java/package/repository/_FortuneRepository.java', javaDir + 'repository/FortuneRepository.java');
-    this.template('src/main/java/package/web/rest/_FortuneResource.java', javaDir + 'web/rest/FortuneResource.java');
-    this.template('src/main/resources/config/liquibase/_fortunes.csv', resourceDir + 'config/liquibase/fortunes.csv');
+    this.template('_swagger2markup.gradle', 'swagger2markup.gradle');
 
-    this.changelogDate = jhipsterFunc.dateFormatForLiquibase();
-    this.template('src/main/resources/config/liquibase/changelog/_added_entity_Fortune.xml', resourceDir + 'config/liquibase/changelog/' + this.changelogDate + '_added_entity_Fortune.xml');
-    jhipsterFunc.addChangelogToLiquibase(this.changelogDate + '_added_entity_Fortune');
+    this.template('src/docs/asciidoc/_index.adoc', 'src/docs/asciidoc/index.adoc');
 
-    this.template('src/main/webapp/scripts/app/fortune/_fortune.controller.js', webappDir + 'scripts/app/fortune/fortune.controller.js');
-    jhipsterFunc.addJavaScriptToIndex('app/fortune/fortune.controller.js');
-    this.template('src/main/webapp/scripts/app/fortune/_fortune.html', webappDir + 'scripts/app/fortune/fortune.html');
-    this.template('src/main/webapp/scripts/app/fortune/_fortune.js', webappDir + 'scripts/app/fortune/fortune.js');
-    jhipsterFunc.addJavaScriptToIndex('app/fortune/fortune.js');
-    this.template('src/main/webapp/scripts/components/fortune/_fortune.service.js', webappDir + 'scripts/components/fortune/fortune.service.js');
-    jhipsterFunc.addJavaScriptToIndex('components/fortune/fortune.service.js');
-    jhipsterFunc.addElementToMenu('fortune', 'sunglasses', true);
-    jhipsterFunc.addElementTranslationKey('fortune', 'Fortune', 'en');
-    jhipsterFunc.addElementTranslationKey('fortune', 'Fortune', 'fr');
+    jhipsterFunc.applyFromGradleScript('swagger2markup');
+    jhipsterFunc.addGradleDependency('testCompile', 'io.springfox', 'springfox-staticdocs', 'springfox_version');
+    jhipsterFunc.addGradlePlugin('org.asciidoctor', 'asciidoctor-gradle-plugin', '1.5.3');
+    jhipsterFunc.addGradlePlugin('org.asciidoctor', 'asciidoctorj-pdf', '1.5.0-alpha.10.1');
 
-    jhipsterFunc.copyI18nFilesByName(this, webappDir, 'fortune.json', 'en');
-    jhipsterFunc.copyI18nFilesByName(this, webappDir, 'fortune.json', 'fr');
     done();
   },
 
