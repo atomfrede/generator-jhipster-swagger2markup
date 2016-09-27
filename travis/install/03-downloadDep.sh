f165c83da61a314eddd82ef60cc16e75647bb0a3
Frederik Hahne <frederik.hahne@gmail.com>
2016-09-27 06:58:51 +0200

    update travis script

--- travis/install/03-downloadDep.sh
+++ travis/install/03-downloadDep.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 set -ev
 #-------------------------------------------------------------------------------
-# Use jhipster-travis-build that contain .m2 and node_modules
+# Check the cache from Travis
 #-------------------------------------------------------------------------------
-if [ $JHIPSTER_MVN_DEP == 1 ]; then
-  cd $TRAVIS_BUILD_DIR/
-  git clone https://github.com/jhipster/jhipster-travis-build.git
-  rm -Rf $HOME/.m2/repository/
-  mv $TRAVIS_BUILD_DIR/jhipster-travis-build/repository $HOME/.m2/
-  mv $TRAVIS_BUILD_DIR/jhipster-travis-build/node_modules/ $JHIPSTER_SAMPLES/$JHIPSTER/
-  ls -al $HOME/.m2/ $HOME/.m2/repository/ $JHIPSTER_SAMPLES/$JHIPSTER/
-#-------------------------------------------------------------------------------
-# Clone official jhipster-sample-app
-#-------------------------------------------------------------------------------
-elif [ $JHIPSTER_MVN_DEP == 2 ]; then
-  cd $TRAVIS_BUILD_DIR/
-  git clone https://github.com/jhipster/jhipster-sample-app.git
-  cd $TRAVIS_BUILD_DIR/jhipster-sample-app/
-  mvn dependency:go-offline
-#-------------------------------------------------------------------------------
-# By default, do nothing
-#-------------------------------------------------------------------------------
+if [ ! -f "$HOME"/.m2/cache.txt ]; then
+    echo "No cache.txt, generate one for the next time"
+    echo "[$(date)] $JHIPSTER" > "$HOME"/.m2/cache.txt
 else
-  echo "By default, no speedup build..."
+    echo "Found cache.txt"
+    cat "$HOME"/.m2/cache.txt
+fi
+
+# no cache for first build
+if [ "$JHIPSTER_NODE_CACHE" == 0 ]; then
+    rm -Rf $HOME/app/node_modules
+    rm -Rf $HOME/app/node
 fi
