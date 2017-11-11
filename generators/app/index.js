63bd7085ea8c321f4d2369d3ee9892a4b9135808
Frederik Hahne <atomfrede@gmail.com>
2017-11-11 23:21:39 +0100

    update lint config

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,25 +1,27 @@
-'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
-var packagejs = require(__dirname + '/../../package.json');
+
+
+const yeoman = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+
+const packagejs = require('../../package.json');
 
 // Stores JHipster variables
-var jhipsterVar = {
+const jhipsterVar = {
   moduleName: 'swagger2markup'
 };
 
 // Stores JHipster functions
-var jhipsterFunc = {};
+const jhipsterFunc = {};
 
 module.exports = yeoman.generators.Base.extend({
 
   initializing: {
-    templates: function (args) {
+    templates(args) {
       this.composeWith('jhipster:modules', {
         options: {
-          jhipsterVar: jhipsterVar,
-          jhipsterFunc: jhipsterFunc
+          jhipsterVar,
+          jhipsterFunc
         }
       });
       if (args === 'default') {
@@ -28,15 +30,13 @@ module.exports = yeoman.generators.Base.extend({
     }
   },
 
-  prompting: function () {
-    var done = this.async();
+  prompting() {
+    const done = this.async();
 
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the ' + chalk.red('JHipster swagger2markup') + ' generator! ' + chalk.yellow('v' + packagejs.version)
-    ));
+    this.log(yosay(`Welcome to the ${chalk.red('JHipster swagger2markup')} generator! ${chalk.yellow('v' + packagejs.version)}`));
 
-    var prompts = [
+    const prompts = [
       {
         type: 'confirm',
         name: 'installAsciidocSample',
@@ -48,8 +48,8 @@ module.exports = yeoman.generators.Base.extend({
         name: 'apiDocResultType',
         message: 'Which file types you would like to generate?',
         choices: [
-          {name: 'HTML5', value: 'html5'},
-          {name: 'PDF', value: 'pdf'}
+          { name: 'HTML5', value: 'html5' },
+          { name: 'PDF', value: 'pdf' }
         ],
         default: ['none']
       },
@@ -67,7 +67,7 @@ module.exports = yeoman.generators.Base.extend({
       this.springRestDocSamples = true;
       done();
     } else {
-      this.prompt(prompts, function (props) {
+      this.prompt(prompts, (props) => {
         this.props = props;
         // To access props later use this.props.someOption;
 
@@ -75,19 +75,19 @@ module.exports = yeoman.generators.Base.extend({
         this.installAsciidocSample = this.props.installAsciidocSample;
         this.springRestDocSamples = this.props.springRestDocSamples;
         done();
-      }.bind(this));
+      });
     }
   },
 
-  writing: function () {
-    var done = this.async();
+  writing() {
+    const done = this.async();
 
     this.packageName = jhipsterVar.packageName;
     this.packageFolder = jhipsterVar.packageFolder;
     this.angularAppName = jhipsterVar.angularAppName;
     this.buildTool = jhipsterVar.buildTool;
     this.mainClass = jhipsterVar.mainClassName;
-    var javaTestDir = 'src/test/java/' + this.packageFolder + '/';
+    const javaTestDir = `src/test/java/${this.packageFolder}/`;
 
     // if no selection, do nothing
     if (this.apiDocResultType.length === 0) {
@@ -105,12 +105,11 @@ module.exports = yeoman.generators.Base.extend({
     }
 
     this.template('src/docs/asciidoc/_index.adoc', 'src/docs/asciidoc/index.adoc');
-    this.template('src/test/java/package/web/rest/_Swagger2MarkupIntTest.java', javaTestDir + 'web/rest/Swagger2MarkupIntTest.java', this, {});
+    this.template('src/test/java/package/web/rest/_Swagger2MarkupIntTest.java', `${javaTestDir}web/rest/Swagger2MarkupIntTest.java`, this, {});
     this.template('src/docs/asciidoc/_index.adoc', 'src/docs/asciidoc/index.adoc');
 
     this.template('src/test/resources/config/_application-s2m.yml', 'src/test/resources/config/application-s2m.yml', this, {});
     if (this.buildTool === 'gradle') {
-
       this.template('gradle/_swagger2markup.gradle', 'gradle/swagger2markup.gradle');
       jhipsterFunc.applyFromGradleScript('gradle/swagger2markup');
       jhipsterFunc.addGradleDependency('testCompile', 'io.springfox', 'springfox-staticdocs', '2.6.1');
@@ -122,10 +121,8 @@ module.exports = yeoman.generators.Base.extend({
       jhipsterFunc.addGradlePlugin('org.asciidoctor', 'asciidoctorj-pdf', '1.5.0-alpha.16');
       jhipsterFunc.addGradlePlugin('io.github.swagger2markup', 'swagger2markup-gradle-plugin', '1.3.1');
       jhipsterFunc.addGradlePlugin('io.github.swagger2markup', 'swagger2markup-spring-restdocs-ext', '1.3.1');
-
     } else if (this.buildTool === 'maven') {
-
-      var swagger2markupPuginDependencies = '                <dependencies>\n' +
+      const swagger2markupPuginDependencies = '                <dependencies>\n' +
                     '                  <dependency>\n' +
                     '                      <groupId>io.github.swagger2markup</groupId>\n' +
                     '                      <artifactId>swagger2markup-spring-restdocs-ext</artifactId>\n' +
@@ -133,19 +130,19 @@ module.exports = yeoman.generators.Base.extend({
                     '                  </dependency>\n' +
                 '                </dependencies>\n';
 
-      var swagger2markupConfiguration = '                <configuration>\n' +
+      let swagger2markupConfiguration = '                <configuration>\n' +
         '                    <outputDir>${project.basedir}/target/docs/asciidoc</outputDir>\n' +
         '                    <swaggerInput>${project.basedir}/target/swagger/swagger.json</swaggerInput>\n' +
         '                    <config>\n' +
         '                       <swagger2markup.pathsGroupedBy>TAGS</swagger2markup.pathsGroupedBy>\n';
 
-        if (this.springRestDocSamples) {
-          swagger2markupConfiguration += '                      <swagger2markup.extensions.springRestDocs.snippetBaseUri>${project.basedir}/target/docs/asciidoc/snippets</swagger2markup.extensions.springRestDocs.snippetBaseUri>\n';
-        }
-        swagger2markupConfiguration += '                    </config>\n';
-        swagger2markupConfiguration += '               </configuration>\n';
+      if (this.springRestDocSamples) {
+        swagger2markupConfiguration += '                      <swagger2markup.extensions.springRestDocs.snippetBaseUri>${project.basedir}/target/docs/asciidoc/snippets</swagger2markup.extensions.springRestDocs.snippetBaseUri>\n';
+      }
+      swagger2markupConfiguration += '                    </config>\n';
+      swagger2markupConfiguration += '               </configuration>\n';
 
-      var pluginDependencies = '                <dependencies>\n' +
+      const pluginDependencies = '                <dependencies>\n' +
         '                    <dependency>\n' +
         '                        <groupId>org.asciidoctor</groupId>\n' +
         '                        <artifactId>asciidoctorj-pdf</artifactId>\n' +
@@ -159,7 +156,7 @@ module.exports = yeoman.generators.Base.extend({
         '                </dependencies>\n';
 
       // Start executions
-      var executions = '                <executions>\n';
+      let executions = '                <executions>\n';
 
       if (this.apiDocResultType.indexOf('html5') !== -1) {
         executions += '                    <execution>\n' +
@@ -192,7 +189,7 @@ module.exports = yeoman.generators.Base.extend({
       executions += '                </executions>\n';
       // End executions
 
-      var asiidoctorjConfiguration = '                <configuration>\n' +
+      const asiidoctorjConfiguration = '                <configuration>\n' +
         '                    <sourceDirectory>${project.basedir}/src/docs/asciidoc</sourceDirectory>\n' +
         '                    <sourceDocumentName>index.adoc</sourceDocumentName>\n' +
         '                    <attributes>\n' +
@@ -205,9 +202,9 @@ module.exports = yeoman.generators.Base.extend({
 
       jhipsterFunc.addMavenDependency('io.springfox', 'springfox-staticdocs', '2.6.1', '<scope>test</scope>');
       if (this.springRestDocSamples) {
-          jhipsterFunc.addMavenDependency('org.springframework.restdocs', 'spring-restdocs-mockmvc', '1.2.2.RELEASE', '<scope>test</scope>');
-          jhipsterFunc.addMavenDependency('org.springframework.restdocs', 'spring-restdocs-core', '1.2.2.RELEASE', '<scope>test</scope>');
-          jhipsterFunc.addMavenDependency('io.github.swagger2markup', 'swagger2markup-spring-restdocs-ext', '1.3.1', '<scope>test</scope>');
+        jhipsterFunc.addMavenDependency('org.springframework.restdocs', 'spring-restdocs-mockmvc', '1.2.2.RELEASE', '<scope>test</scope>');
+        jhipsterFunc.addMavenDependency('org.springframework.restdocs', 'spring-restdocs-core', '1.2.2.RELEASE', '<scope>test</scope>');
+        jhipsterFunc.addMavenDependency('io.github.swagger2markup', 'swagger2markup-spring-restdocs-ext', '1.3.1', '<scope>test</scope>');
       }
       jhipsterFunc.addMavenPlugin('io.github.swagger2markup', 'swagger2markup-maven-plugin', '1.3.1', swagger2markupPuginDependencies + swagger2markupConfiguration);
       jhipsterFunc.addMavenPlugin('org.asciidoctor', 'asciidoctor-maven-plugin', '1.5.6', executions + pluginDependencies + asiidoctorjConfiguration);
@@ -216,7 +213,7 @@ module.exports = yeoman.generators.Base.extend({
     done();
   },
 
-  install: function () {
+  install() {
     // No op required here currently
   }
 });
